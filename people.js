8be769ddb0e70cccd20ca6a3251d29ad29333ab1
danirando <danielerando@tiscali.it>
2025-05-08 13:59:50 +0200

    import data in people.js

--- people.js
+++ people.js
@@ -1,2 +1,14 @@
 const { fullName } = require("./names");
 const { hobbies } = require("./hobbies");
+
+const init = () => {
+  const person = {
+    fullName,
+    hobbies,
+  };
+  return person;
+};
+
+let person = init();
+
+console.log(person);
